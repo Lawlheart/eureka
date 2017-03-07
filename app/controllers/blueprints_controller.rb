2c49b52bcb9c565cf12eb40d2ffa9ef5897aa593
Kenneth Black <kenneth.black@live.com>
2017-03-07 15:53:54 -0800

    add more blueprint styles, implement blueprint creation with AJAX.

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -1,5 +1,6 @@
 class BlueprintsController < ApplicationController
-  before_action :get_blueprint, only: [:show]
+  before_action :get_blueprint, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @blueprints = Blueprint.all
@@ -8,8 +9,31 @@ class BlueprintsController < ApplicationController
   def show
   end
 
+  def new
+    @blueprint = Blueprint.new
+  end
+
+  def create
+    @blueprint = Blueprint.new(blueprint_params.merge(user_id: current_user.id))
+    respond_to do |format|
+      if @blueprint.save!
+
+        format.html { redirect_to blueprints_path, notice: 'Blueprint Saved Successfully'}
+        format.js
+      else
+        format.html { render :new, alert: 'There was an error.' }
+        format.js { redirect_back(fallback_location: blueprints_path, notice: 'There was an error.')}
+      end
+    end
+
+  end
+
 private
   def get_blueprint
     @blueprint = Blueprint.find(params[:id])
+  end
+
+  def blueprint_params
+    params.require(:blueprint).permit(:name, :description, :source, :source_url, :demo_url)
   end
 end
