4e84a892f34f879a295fe93ffd1b73e6e0d19ce9
Kenneth Black <kenneth.black@live.com>
2017-03-06 16:22:48 -0800

    add designs from html5up.net

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -1,5 +1,15 @@
 class BlueprintsController < ApplicationController
+  before_action :get_blueprint, only: [:show]
+
   def index
     @blueprints = Blueprint.all
+  end
+
+  def show
+  end
+
+private
+  def get_blueprint
+    @blueprint = Blueprint.find(params[:id])
   end
 end
